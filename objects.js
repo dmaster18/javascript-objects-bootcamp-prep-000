da83f82710d1511e1050fb613394ac4c6e1643c6
David Strauch <dmaster18@gmail.com>
2019-06-11 19:17:41 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -6,4 +6,7 @@ function updatePlaylist (obj, key, value) {
   return Object.assign({}, obj, {[key]: value});
 }
 
-function removeFromPlaylist(obj, key)
+function removeFromPlaylist(obj, key) {
+  delete
+  
+}
