ad015beeefad5f703ab7a3828be0e02b98da8235
David Strauch <dmaster18@gmail.com>
2019-06-11 19:39:28 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -3,7 +3,7 @@ var playlist = {
 }
 
 function updatePlaylist (playlist, artistName, songTitle) {
- return artistName
+ return plalist.artistName 
 }
 
 function removeFromPlaylist(playlist, artistName) {
